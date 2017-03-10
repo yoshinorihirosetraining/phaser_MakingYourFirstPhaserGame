7e9a09dd216171121b38a7058876bcfe0c3fc153
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-03-10 21:05:28 +0900

    4. Groups

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
-    game.load.image('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 }
 
