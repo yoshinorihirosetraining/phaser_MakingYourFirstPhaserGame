0da29efd39671772508824256439b06854c4447d
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-03-10 20:54:38 +0900

    4. Groups

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function preload() {
 }
 
 var platform;
+var player;
 
 function create() {
 
@@ -43,6 +44,21 @@ function create() {
     ledge = platforms.create(-150, 250, 'ground');
 
     ledge.body.immovable = true;
+
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+    // We need to enable physics on the player
+    game.physics.arcade.enable(player);
+
+    // Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    // Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
 
 }
 
